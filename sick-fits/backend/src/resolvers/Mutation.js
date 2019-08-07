e434c5c4b35a85fb0b4e7e2976dc1f7588f96927
Nico Bermudez <nicolas.bermudez@ef.com>
2019-08-07 10:49:23 -0400

    Update resolvers and schema for delete function

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -7,6 +7,25 @@ const Mutation = {
         }, info);
 
         return item;
+    },
+    updateItem(parent, args, ctx, info) {
+        // Copy updates
+        const updates = { ...args };
+        // Remove ID from updates
+        delete updates.id;
+        // Update
+        return ctx.db.mutation.updateItem({
+            data: updates,
+            where: {
+                id: args.id
+            },
+        }, info);
+    },
+    async deleteItem(parent, args, ctx, info) {
+        const where = { id: args.id };
+        // Find item, check if user owns item, delete
+        const item = await ctx.db.query.item({ where }, `{ id title }`);
+        return ctx.db.mutation.deleteItem({ where }, info);
     }
 };
 
