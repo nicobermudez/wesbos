257e79fe24f56367da511fa3c0b400898428cd42
Nico Bermudez <nicolas.bermudez@ef.com>
2019-08-07 10:25:57 -0400

    Update and Delete items

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -57,7 +57,7 @@ const Header = () => (
     <StyledHeader>
         <div className="bar">
             <Logo>
-                <Link href="">
+                <Link href="/">
                     <a>Sick Fits</a>
                 </Link>
             </Logo>
