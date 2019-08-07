7746b60a2d08bc33f60a4a23e99448e0feaf6e19
Nico Bermudez <nicolas.bermudez@ef.com>
2019-08-07 18:09:44 -0400

    user flow

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs');
 const JWT = require('jsonwebtoken')
+const { randomBytes } = require('crypto');
+const { promisify } = require('util');
 
 const Mutation = {
     async createItem(parent, args, ctx, info) {
@@ -51,7 +53,86 @@ const Mutation = {
             maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
         });
         return user;
-    }
+    },
+    async signin(parent, { email, password }, ctx, info) {
+        // check if there is a user with that login info
+        const user = await ctx.db.query.user({ where: { email }});
+        if(!user) {
+            throw new Error(`No such user found for email ${email}`);
+        }
+        // check if password is correct
+        const valid = await bcrypt.compare(password, user.password)
+        if(!valid) {
+            throw new Error('Invalid Password!');
+        }
+        // create JWT token 
+        const token = JWT.sign({ userId: user.id }, process.env.APP_SECRET);
+        // set jwt as cookie on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+        });
+        return user;
+    },
+    signout(parent, args, ctx, info) {
+        ctx.response.clearCookie('token');
+        return { message: 'Goodbye' }
+    },
+    async requestReset(parent, args, ctx, info) {
+        // check if this is a real user
+        const user = await ctx.db.query.user({ where: { email: args.email } });
+        if(!user) {
+            throw new Error(`No such user found for email ${args.email}`);
+        }
+        // set reset token and expiry
+        const randomBytesPromisified = promisify(randomBytes);
+        const resetToken = (await randomBytesPromisified(20)).toString('hex');
+        const resetTokenExpiry = Date.now() + 3600000;
+        const res = await ctx.db.mutation.updateUser({
+            where: { email: args.email },
+            data: { resetToken, resetTokenExpiry }
+        })
+        console.log(res)
+        return { message: "Thanks"}
+        // email them reset token
+    },
+    async resetPassword(parent, args, ctx, info) {
+        // 1. check if the passwords match
+        if (args.password !== args.confirmPassword) {
+          throw new Error("Yo Passwords don't match!");
+        }
+        // 2. check if its a legit reset token
+        // 3. Check if its expired
+        const [user] = await ctx.db.query.users({
+          where: {
+            resetToken: args.resetToken,
+            resetTokenExpiry_gte: Date.now() - 3600000,
+          },
+        });
+        if (!user) {
+          throw new Error('This token is either invalid or expired!');
+        }
+        // 4. Hash their new password
+        const password = await bcrypt.hash(args.password, 10);
+        // 5. Save the new password to the user and remove old resetToken fields
+        const updatedUser = await ctx.db.mutation.updateUser({
+          where: { email: user.email },
+          data: {
+            password,
+            resetToken: null,
+            resetTokenExpiry: null,
+          },
+        });
+        // 6. Generate JWT
+        const token = JWT.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+        // 7. Set the JWT cookie
+        ctx.response.cookie('token', token, {
+          httpOnly: true,
+          maxAge: 1000 * 60 * 60 * 24 * 365,
+        });
+        // 8. return the new user
+        return updatedUser;
+      },
 };
 
 module.exports = Mutation;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -4,6 +4,14 @@ const Query = {
     items: forwardTo('db'),
     item: forwardTo('db'),
     itemsConnection: forwardTo('db'),
+    me(parent, args, ctx, info) {
+        // check if current user
+        if (!ctx.request.userId) {
+            return null;
+        }
+        return ctx.db.query.user({where: { id: ctx.request.userId }, 
+        }, info);
+    }
 };
 
 module.exports = Query;
