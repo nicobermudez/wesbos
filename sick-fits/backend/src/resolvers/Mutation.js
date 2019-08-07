64073d65eacc67fc08073ab69136f14ee0910539
Nico Bermudez <nicolas.bermudez@ef.com>
2019-08-07 15:07:37 -0400

    Pagination and backend signup

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const JWT = require('jsonwebtoken')
+
 const Mutation = {
     async createItem(parent, args, ctx, info) {
         const item = await ctx.db.mutation.createItem({
@@ -26,6 +29,28 @@ const Mutation = {
         // Find item, check if user owns item, delete
         const item = await ctx.db.query.item({ where }, `{ id title }`);
         return ctx.db.mutation.deleteItem({ where }, info);
+    },
+    async signup(parent, args, ctx, info) {
+        // lowercase email
+        args.email = args.email.toLowerCase();
+        // hash password
+        const password = await bcrypt.hash(args.password, 10);
+        // create user in db
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password,
+                permissions: { set: ['USER'] }
+            }
+        }, info);
+        // create JWT token 
+        const token = JWT.sign({ userId: user.id }, process.env.APP_SECRET);
+        // set jwt as cookie on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+        });
+        return user;
     }
 };
 
