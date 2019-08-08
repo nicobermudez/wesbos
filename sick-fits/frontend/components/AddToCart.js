21f5ba07c42d77e087aa2438fc0ecdb1833f6b7a
Nico Bermudez <nicolas.bermudez@ef.com>
2019-08-08 16:53:36 -0400

    Shopping cart and payments frontend

--- sick-fits/frontend/components/AddToCart.js
+++ sick-fits/frontend/components/AddToCart.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag'
+import { CURRENT_USER_QUERY } from './User'
 
 const ADD_TO_CART_MUTATION = gql` 
     mutation addToCart($id: ID!) {
@@ -15,9 +16,13 @@ class AddToCart extends Component {
     render() {
         const { id } = this.props;
         return (
-            <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ id }}>
-                {(addToCart) => (
-                    <button onClick={addToCart}>Add To Cart</button>
+            <Mutation 
+                refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+                mutation={ADD_TO_CART_MUTATION} 
+                variables={{ id }}
+            >
+                {(addToCart, { error, loading }) => (
+                    <button disabled={loading} onClick={addToCart}>Add {loading ? "ing" : ""} To Cart</button>
                 )}
             </Mutation>
         )
